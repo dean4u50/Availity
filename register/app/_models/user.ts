7e45948040e6b97feb161f7fc09b2ee2c2c03189
Lyle Griess <lgriess@gmail.com>
2022-11-16 22:49:54 -0600

    55

---
+++ register/app/_models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    npi: string;
+    email: string;
+    bus_address: string;
+    token: string;
+}
